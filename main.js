7b649804c27f3805c58430b7e4d3bb468690f8d1
Anuj <ianujsingh.1801@gmail.com>
2025-07-30 23:05:01 +0530

    Updated backend logic

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 import dotenv from 'dotenv';
 import axios from 'axios';  // ✅ Import axios
 
-const API_BASE = "http://localhost:3000";  // or your deployed server
+const API_BASE = import.meta.env.SERVER_API ;
 
-const contactForm = document.querySelector('#contactForm');
-
- document.querySelector("#submitBtn").onClick(alert("contact-form"))
+const contactForm = document.getElementById('contactForm');
 
 if (contactForm) {
   contactForm.addEventListener('submit', async (event) => {
     event.preventDefault();
-    
-    alert("hello anuj")
 
     const name = contactForm.querySelector('#name').value.trim();
     const email = contactForm.querySelector('#email').value.trim();
@@ -24,7 +20,7 @@ if (contactForm) {
       alert("Please enter a valid email address.");
     } else {
       try {
-        const response = await axios.post(`${API_BASE}/sendmessage`, {
+        const response = await axios.post(`${API_BASE}/sendmessage/send`, {
           name,
           email,
           message
