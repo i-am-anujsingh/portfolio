d3974fcad0bbb3ec1fd7e8c676b957cedbfff227
Anuj Singh <ianujsingh.1801@gmail.com>
2026-06-05 09:32:01 +0530

    ...

--- src/components/cards/ProjectCard.jsx
+++ src/components/cards/ProjectCard.jsx
@@ -13,8 +13,8 @@ const ProjectCard = ({children, project}) => {
       <p className='text-sm text-gray-300 py-4'><b className='text-grad'>Role:</b> {project.role}</p>
       <p className='text-sm text-gray-300 py-3'><b className='text-grad'>Technology:</b> {project.technology}</p>
       <div className="w-full h-auto p-4 flex justify-around m-auto">
-        <a className='md:text-2xl' href={project.repo} target='_blank'>View on GitHub</a>
-        <a className='md:text-2xl' href={project.weblink} target='_blank'>Live Demo</a>
+        <a className='md:text-2xl' href={project.repo} onClick={()=>{project.repo? null : alert("Link Not Availabe!")}} target='_blank'>View on GitHub</a>
+        <a className='md:text-2xl' href={project.weblink} onClick={()=>{project.weblink? null : alert("Link Not Availabe!")}} target='_blank'>Live Demo</a>
       </div>
       {/* Control Buttons for Projects */}
       <ControlBtns place="md:bottom-[-5%] bottom-[-20%]" id={project._id} type={"project"} obj={project} fn={DeletingProjects}/>
